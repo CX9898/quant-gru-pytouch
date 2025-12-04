2010bc872efbc355b6ea96a44d5a84ce27ed15c6
Chengxing Zou <zcx5613@gmail.com>
2025-12-04 16:45:28 +0800

    update test

--- pytorch/run_test.sh
+++ pytorch/run_test.sh
@@ -3,8 +3,10 @@ mkdir build
 cd build
 cmake ..
 make -j 10
-cd ../pytouch
+cd ../pytorch
 
 python setup.py build_ext --inplace
 
 python test_custom_gru_quantization.py
+
+python test_gru_train.py
